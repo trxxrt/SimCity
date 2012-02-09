dd2e8fa9db749e06c43292e633005ae8a2c791cd
Loïc Dalloz <osblouf@gmail.com>
2012-02-09 14:07:46 +0100

    raz des fichiers pour refonte de l'algo

--- src/controller/network.c
+++ src/controller/network.c
@@ -10,34 +10,6 @@
 **/
 void net_maj(t_case** tab, int t)
 {
-	//ressources
-	int i, j;
-
-	//si c'est le réseau routier :
-	if(t==ROAD)
-	{
-		//on cherche les routes non marqué pour lancer l'algo :
-		for(i=0; i<size_map; i++)
-			for(j=0; j<size_map; j++)
-			{
-				if((tab[i][j]->state == ROAD) && (tab[i][j]->state_m != 1))//TODO
-					//appel de la fonction du réseau TODO
-			}
-
-	else
-	{
-		//on cherche les centrales :
-		for(i=0; i<size_map; i++)
-			for(j=0; j<size_map; j++)
-			{
-				if(t==WATER)
-					if(tab[i][j]->state)//TODO
-						//appel de la fonction du réseau TODO
-				else if(t==ELEC)
-					if(tab[i][j]->state)//TODO
-						//appel de la fonction du réseau TODO
-			}
-	}
 }
 
 /**
@@ -50,5 +22,4 @@ void net_maj(t_case** tab, int t)
 **/
 void net_run(t_case** tab, int t)
 {
-	//ressources
-	
+}
